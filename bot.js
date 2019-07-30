c333aa96ebe1db628414c4927cf1cf8166941fc3
Alexis King <alexis.king13@my.stjohns.edu>
2019-07-30 19:58:00 -0400

    baby girl is working now

--- bot.js
+++ bot.js
@@ -1,13 +1,7 @@
 var Discord = require('discord.io');
-var logger = require('winston');
 var auth = require('./auth.json');
 
-// Configure logger settings
-logger.remove(logger.transports.Console);
-logger.add(new logger.transports.Console, {
-    colorize: true
-});
-logger.level = 'debug';
+
 
 // Initialize Discord Bot
 var client = new Discord.Client({
@@ -21,18 +15,16 @@ const dbtimeDif = 4;
 
 //bot is logged in
 client.on("ready", () => {
- logger.info('Logged in as: ');
-    logger.info(client.username + ' - (' + client.id + ')');
+ console.log("Buller is online");
 });
 
 //imitating how vlv had forte listen for commands
 //todo clean
-client.on("message", (message) => {
- if(!message.startsWith(prefix)) return;
- if (message.author.bot) return;
- if (message.channel.type !== "text") return;
- if(message.content.startsWith(prefix + "dbtime")){
-  var momodate = new Date();
-  message.channel.send( "dbtime is currently: " + momodate.getHours()-dbtimeDif + ":"+ momodate.getMinutes());
+client.on("message", (user, userID, channelID, message, evt) => {
+ if(message.startsWith(prefix+"dbtime")){
+	var momodate = new Date();
+	momodate.setUTCHours(momodate.getHours()-dbtimeDif);
+	var dbtime = "dbtime is currently: "+ momodate.getHours()+ ":"+ momodate.getMinutes();
+	client.sendMessage({to: channelID, message: dbtime});
  }
 });
